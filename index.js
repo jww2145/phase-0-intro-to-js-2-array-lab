29df2385d57b5a3c71ca32bbac653b86cd63bf76
Joshua Wu <jww2145@columbia.edu>
2022-05-27 20:46:01 -0400

    commit tree a3078a7f83c439e01722f3cb3c514cc15a340a7a

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    let newCats = [...cats ,"Broom"]
+    return newCats
+}
+
+function prependCat(name){
+    let newCats = ["Arnold", ...cats]
+    return newCats
+}
+
+function removeLastCat(){
+    let newCats = cats.slice(0, -1)
+    return newCats
+}
+
+function removeFirstCat(){
+    let newCats = cats.slice(1,cats.length)
+    return newCats
+}
